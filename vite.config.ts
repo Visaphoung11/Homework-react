fd615dcbb77b87a8b35b994116792d35316f56f9
viaslphoung <visalphoung22@gmail.com>
2025-04-08 22:04:34 +0700

    Update Vite config for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import lineClamp from "@tailwindcss/line-clamp";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss({ plugins: [lineClamp] }), react()],
+  plugins: [tailwindcss(), react()],
+  base: "/Homework-react/", // This should be your GitHub repository name
 });
